b121125269f0b6a447a7838ba7a591240cfe8035
imeuro <mauro.fioravanzi@gmail.com>
2017-04-04 00:22:59 +0200

    fancy and dirty way to i2c data

--- boxes/inside.php
+++ boxes/inside.php
@@ -1,1 +1,7 @@
 <h2>inside</h2>
+<?php
+$temperature = `gpio -x bmp180:100 aread 100` / 10 ;
+$pressure = `gpio -x bmp180:100 aread 101` / 10 ;
+?>
+<time><?php echo $temperature; ?>&deg;</time>
+<time><?php echo $pressure; ?>&deg;</time>
