1a6f4ca0f065fa9ec0aa737d04dd2bcc61ee74e5
imeuro <mauro.fioravanzi@gmail.com>
2017-04-08 17:42:54 +0200

    dyn chart

--- boxes/inside.php
+++ boxes/inside.php
@@ -4,11 +4,11 @@ $temperature = `gpio -x bmp180:100 aread 100` / 10 ;
 $pressure = `gpio -x bmp180:100 aread 101` / 10 ;
 //$humidity = '';
 ?>
-<h3 class="temp" data-temp="<?php echo rand(-30,30); ?>">
+<h3 class="temp" data-temp="<?php echo $temperature; ?>">
 	<?php echo $temperature; ?>&deg;
 	<small>temperature</small>
 </h3>
-<h3 class="pressure">
+<h3 class="pressure" data-pressure="<?php echo $pressure; ?>">
 	<?php echo $pressure; ?>mbar
 	<small>pressure</small>
 </h3>
