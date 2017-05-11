6fa1a39368c7c0a1d6322654e044094b2df1e5bb
imeuro <mauro.fioravanzi@gmail.com>
2017-05-12 00:43:28 +0200

    news source

--- boxes/news.php
+++ boxes/news.php
@@ -1,13 +1,18 @@
 <h2>news</h2>
 <?php
-$myXMLData = file_get_contents('http://feeds.ilpost.it/ilpost?format=xml');
+$myXMLData = file_get_contents('http://www.repubblica.it/rss/homepage/rss2.0.xml');
+// $myXMLData = file_get_contents('http://feeds.ilpost.it/ilpost?format=xml');
 
 $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
 $news_item = $xml->channel->item;
-//print_r($news_item);
+// print_r($news_item);
 echo "<ul>";
+$i = 1;
 foreach ($news_item as $news) {
-	echo '<li>'.$news->title.'</li>';
+	if ($i <= 5) :
+		echo '<li>'.$news->title.'</li>';
+		$i++;
+	endif;
 }
 echo "</ul>";
 ?>
