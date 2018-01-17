cce9c668677b066046e6c4befd9727ccd4eabcc4
Mauro Fioravanzi <mauro.fioravanzi@mondadori.it>
2018-01-17 18:59:59 +0100

    stats wip

---
+++ boxes/rob_stats.php
@@ -0,0 +1,74 @@
+<?php
+function httpPost($url,$params)
+{
+  $postData = '';
+   //create name value pairs seperated by &
+   foreach($params as $k => $v)
+   {
+      $postData .= $k . '='.$v.'&';
+   }
+   $postData = rtrim($postData, '&');
+
+    $ch = curl_init();
+
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, count($postData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+    $output=curl_exec($ch);
+
+    curl_close($ch);
+    return $output;
+
+}
+
+$begin_M = date('n',strtotime("-1 month"));
+if ($begin_M == '12') {
+	$begin_Y = date('Y',strtotime("-1 year"));
+} else {
+	$begin_Y = date('Y');
+}
+
+$params = array(
+	"job" => "12",
+	"month_begin" => $begin_M,
+	"month_end" => date('n'),
+	"year_begin" => $begin_Y,
+	"year_end" => date('Y'),
+	"user_no" => "2251088"
+);
+
+$profiledata = httpPost("https://www.gran-turismo.com/us/api/gt7sp/profile/",$params);
+$GTapi = json_decode($profiledata);
+
+$i = 0;
+$l = 0;
+$GTdata_DR = '';
+$GTdata_SR = '';
+foreach ($GTapi->stats_history as $GTdata) {
+	foreach ($GTdata->stats12 as $GTdata_stats) {
+		if ($GTdata_stats != '0'){
+			$GTdata_DR .= "{x: ".$i.", y: ".$GTdata_stats."},\n";
+			$i++;
+		}
+	}
+	foreach ($GTdata->stats13 as $GTdata_stats) {
+		if ($GTdata_stats != '0'){
+			$GTdata_SR .= "{x: ".$l.", y: ".$GTdata_stats."},\n";
+			$l++;
+		}
+	}
+}
+
+
+$GTdata_DR = substr($GTdata_DR, 0, -2);
+$GTdata_SR = substr($GTdata_SR, 0, -2);
+
+echo $GTdata_DR;
+echo "\n\n\n---------------\n\n\n";
+echo $GTdata_SR;
+
+//print_r($GTapi->stats_history);
+?>
