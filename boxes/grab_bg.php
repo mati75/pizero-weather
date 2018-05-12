55dc5d9c72bb572885f6104cdb6082324c31b455
imeuro <mauro.fioravanzi@gmail.com>
2018-05-12 19:40:42 +0200

    scandir fix (?)

--- boxes/grab_bg.php
+++ boxes/grab_bg.php
@@ -1,12 +1,14 @@
 <?php
 // we have a local folder, choose a random image in it:
-$dir = "backgrounds/";
-$images = scandir($dir);
-$i = rand(2, sizeof($images)-1);
+$dir = "../backgrounds/";
+if(scandir($dir) == false) {
+	echo 'falZo';
+	die();
+} else {
+	$images = scandir($dir);
+	$i = rand(2, sizeof($images)-1);
+}
 ?>
 	<script>
 		document.getElementById('pagecontent').setAttribute("style", "background-image: url('<?php echo $dir.'/'.$images[$i] ?>');");
 	</script>
-<?php
-}
-?>
